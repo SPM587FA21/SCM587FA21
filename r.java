b14b378d9be498c669c204aff625beb0ec2efee6
AbhishekBonageri <abhishek841998@gmail.com>
2021-10-31 12:10:07 -0500

    b.java and r.java commited

--- r.java
+++ r.java
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 RFA21SCM18C
+=======
+<<<<<<< HEAD
+RFA21SCM18C
+=======
+/*     RFA21SCM18C     */
+>>>>>>> 048149d (Revert "“Fixes #232. RFA21SCM18C first name added”")
+>>>>>>> 2d64a52 (Revert "“Fixes #232. RFA21SCM18C first name added”")
