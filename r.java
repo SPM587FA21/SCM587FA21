489e0eb9018dcb66d4455d88d2ca38e03cfad50f
AFA21SCM21BO <89863055+AFA21SCM21BO@users.noreply.github.com>
2021-11-01 14:54:36 -0500

    Update r.java

--- r.java
+++ r.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 RFA21SCM18C
-=======
-<<<<<<< HEAD
 RFA21SCM18C
-=======
-/*     RFA21SCM18C     */
->>>>>>> 048149d (Revert "“Fixes #232. RFA21SCM18C first name added”")
->>>>>>> 2d64a52 (Revert "“Fixes #232. RFA21SCM18C first name added”")
+
